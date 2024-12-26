20ca25a79e5dd6d390b6947c08e5a5c6b90c138e
reonokiy <reonokiy@gmail.com>
2024-12-27 01:28:48 +0800

    add fly0 wireguard

--- hosts/buno/networking.nix
+++ hosts/buno/networking.nix
@@ -1,4 +1,8 @@
+{ config, ... }:
+
 {
+  sops.secrets."fly0/private_key" = { };
+
   boot.kernel.sysctl = {
     "net.ipv4.ip_forward" = 1;
     "net.ipv6.conf.all.forwarding" = 1;
@@ -22,5 +26,20 @@
     enable = true;
     externalInterface = "eth0";
     internalInterfaces = [ "eth1" ];
+  };
+
+  networking.wg-quick.interfaces.fly0 = {
+    autostart = true;
+    privateKeyFile = config.sops.secrets."fly0/private_key".path;
+    address = [ "fdaa:9:7373:a7b:163a:0:a:2/120" ];
+    dns = [ "fdaa:9:7373::3" ];
+    peers = [
+      {
+        publicKey = "DBn8uXwmcc4A06//C0T3bX9gjNp0eh/EzS4MDjnSKhc=";
+        allowedIPs = [ "fdaa:9:7373::/48" ];
+        endpoint = "fra1.gateway.6pn.dev:51820";
+        persistentKeepalive = 15;
+      }
+    ];
   };
 }
