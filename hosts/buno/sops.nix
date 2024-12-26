50c144b353bc58c16c2bee661d3f961bbf6e925f
reonokiy <reonokiy@gmail.com>
2024-12-26 22:07:05 +0800

    update tailscale config

--- hosts/buno/sops.nix
+++ hosts/buno/sops.nix
@@ -1,4 +1,5 @@
 {
   sops.defaultSopsFile = ../../secrets/buno.yaml;
+  sops.defaultSopsFormat = "yaml";
   sops.age.keyFile = "/var/lib/sops-nix/key.txt";
 }
--- hosts/buno/tailscale.nix
+++ hosts/buno/tailscale.nix
@@ -1,7 +1,14 @@
+{ config, ... }:
+
 {
+  sops.secrets."tailscale/auth_key" = { };
+
   services.tailscale = {
     enable = true;
     interfaceName = "tailscale0";
     openFirewall = true;
+    authKeyFile = config.sops.secrets."tailscale/auth_key".path;
+    disableTaildrop = true;
+    useRoutingFeatures = "both";
   };
 }
